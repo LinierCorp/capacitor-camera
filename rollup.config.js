879301236754ea60dabe57bc621050cf8534a266
Benjamin LINIER <benjamin.linier@risques-developpement.fr>
2023-09-01 02:19:20 +0200

    ok

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorCamera',
+      name: 'LinierCamera',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
